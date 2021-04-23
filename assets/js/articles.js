f8e3f39468a2b34aab090abc47530858a9d09b7b
Kenneth Fullbright (フルブライト　ケネス) <74458502+kennyneedsmilky@users.noreply.github.com>
2021-04-23 14:18:27 +0900

    Add files via upload

---
+++ assets/js/articles.js
@@ -0,0 +1,117 @@
+/* ケンミル */ /* 神様最高 */ "use strict"; console.log("articles.js");
+
+const placeHolderArticles = {
+    "status": "ok",
+    "totalResults": 9724,
+    "articles": [
+        {
+            "source": {
+                "id": "the-times-of-india",
+                "name": "The Times of India"
+            },
+            "author": "ETtech",
+            "title": "Covid's unsung heroes",
+            "description": "In today's ETtech Morning Dispatch: E-commerce companies go on another hiring spree; health-tech startups join the vaccination drive; and Nasscom gets its first woman chairperson.",
+            "url": "https://economictimes.indiatimes.com/tech/newsletters/morning-dispatch/covids-unsung-heroes/articleshow/82207572.cms",
+            "urlToImage": "https://img.etimg.com/thumb/msid-82207572,width-1070,height-580,imgsize-1497793,overlay-ettech/photo.jpg",
+            "publishedAt": "2021-04-23T02:43:11Z",
+            "content": "Good morning,The term frontline worker conjures up images of women and men in protective equipment, saving peoples lives. It doesnt usually bring to mind the person who delivered that Macbook to you … [+9106 chars]"
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Electrek"
+            },
+            "author": "Fred Lambert",
+            "title": "Tesla increases Model 3 and Model Y prices again, now starts at $39,000",
+            "description": "Tesla Model 3 and Model Y prices have been on a roller coaster so far in 2021 and now they went up again in a new update today.\nThe cheapest Tesla now starts at $39,000.\n more…\nThe post Tesla increases Model 3 and Model Y prices again, now starts at $39,000 a…",
+            "url": "https://electrek.co/2021/04/22/tesla-increases-model-3-y-prices-again/",
+            "urlToImage": "https://i2.wp.com/electrek.co/wp-content/uploads/sites/3/2019/11/Tesla-Model-S-X-3-hero.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1",
+            "publishedAt": "2021-04-23T02:41:42Z",
+            "content": "Tesla Model 3 and Model Y prices have been on a roller coaster so far in 2021 and now they went up again in a new update today.\r\nThe cheapest Tesla now starts at $39,000.\r\nWe have been tracking price… [+2709 chars]"
+        
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "CNBC"
+            },
+            "author": "Evelyn Cheng",
+            "title": "Tesla branded as 'arrogant' in China as pressure mounts on electric carmaker",
+            "description": "Tesla has been a poster child for China's efforts to demonstrate it's letting more foreign businesses operate in its market. But with success comes scrutiny.",
+            "url": "https://www.cnbc.com/2021/04/23/tesla-in-china-pressure-mounts-on-carmaker-after-customer-protest.html",
+            "urlToImage": "https://image.cnbcfm.com/api/v1/image/106555483-15906759672020-05-13t014453z_710781449_rc2dng93w4em_rtrmadp_0_health-coronavirus-tesla-texas.jpeg?v=1600783514",
+            "publishedAt": "2021-04-23T02:36:00Z",
+            "content": "Tesla CEO Elon Musk speaks at an opening ceremony for Tesla China-made Model Y program in Shanghai on Jan. 7.\r\nBEIJING and GUANGZHOU, China Tesla faces mounting pressure in China as state media and r… [+4774 chars]"
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Theregister.com"
+            },
+            "author": "Katyanna Quach",
+            "title": "Look, Ma: No human! Boffins turn on Tesla Autopilot with string, a weight, a seat belt, then watch go for a spin",
+            "description": "Rivals auto-autos snap photos to ensure an actual human is behind the wheel\nTesla vehicles can be easily tricked into driving in Autopilot mode when no-one is sitting on the driver seat or controlling the steering wheel, according to a new experiment conducte…",
+            "url": "https://www.theregister.com/2021/04/23/tesla_autopilot_fooled/",
+            "urlToImage": "https://regmedia.co.uk/2021/04/23/tesla.jpg",
+            "publishedAt": "2021-04-23T02:33:18Z",
+            "content": "Tesla vehicles can be easily tricked into driving in Autopilot mode when no-one is sitting on the driver seat or controlling the steering wheel, according to a new experiment conducted by advocacy gr… [+2605 chars]"
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "CleanTechnica"
+            },
+            "author": "Johnna Crider",
+            "title": "Despite The Protest, Tesla Is Still Hot in China",
+            "description": "If you haven’t been keeping up with the news regarding Tesla and a protester in China, it’s a really sad situation where a customer felt that she had to take extreme actions to get Tesla’s attention. Currently, the Shanghai Auto Show is in full swing. The cus…",
+            "url": "https://cleantechnica.com/2021/04/22/despite-the-protest-tesla-is-still-hot-in-china/",
+            "urlToImage": "https://cleantechnica.com/files/2020/06/Tesla-Model-Y-China.png",
+            "publishedAt": "2021-04-23T02:07:00Z",
+            "content": "If you haven’t been keeping up with the news regarding Tesla and a protester in China, it’s a really sad situation where a customer felt that she had to take extreme actions to get Tesla’s attention.… [+2034 chars]"
+        
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Slashdot.org"
+            },
+            "author": "BeauHD",
+            "title": "Consumer Reports Shows Tesla Autopilot Works With No One In the Driver's Seat",
+            "description": "Rei_is_a_dumbass shares a report from Ars Technica: Last Saturday, two men died when a Tesla Model S crashed into a tree in a residential neighborhood. Authorities said they found no one in the driver's seat -- one man was in the front passenger seat, while t…",
+            "url": "https://tech.slashdot.org/story/21/04/22/2057212/consumer-reports-shows-tesla-autopilot-works-with-no-one-in-the-drivers-seat",
+            "urlToImage": "https://a.fsdn.com/sd/topics/transportation_64.png",
+            "publishedAt": "2021-04-23T02:02:00Z",
+            "content": "Last Saturday, two men died when a Tesla Model S crashed into a tree in a residential neighborhood. Authorities said they found no one in the driver's seat -- one man was in the front passenger seat,… [+1324 chars]"
+        
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "Digital Trends"
+            },
+            "author": "Trevor Mogg",
+            "title": "Tesla’s Autopilot can be easily tricked, engineers find",
+            "description": "Engineers at Consumer Reports said this week that it’s easy to trick Tesla’s Autopilot system into thinking someone is in the driver’s seat.",
+            "url": "https://www.digitaltrends.com/cars/teslas-autopilot-can-be-easily-tricked-engineers-find/",
+            "urlToImage": "https://img.dtcn.com/image/digitaltrends/tesla-preview-image-ronan-glon-1200x0-1200x630-c-ar1.91.jpg",
+            "publishedAt": "2021-04-23T02:00:02Z",
+            "content": "Engineers at Consumer Reports (CR) said this week they were able to “easily” trick Teslas Autopilot system into thinking someone was in the drivers seat, meaning the car could be driven without anyon… [+3380 chars]"
+        
+        },
+        {
+            "source": {
+                "id": null,
+                "name": "DW (English)"
+            },
+            "author": "Deutsche Welle (www.dw.com)",
+            "title": "Tesla cars can be tricked into driverless driving: report",
+            "description": "Consumer Reports' investigation comes after a Tesla crashed into a tree in Texas, killing both men in the car. Authorities say neither of the victims were in the driver's seat at the time of the accident.",
+            "url": "https://www.dw.com/en/tesla-cars-can-be-tricked-into-driverless-driving-report/a-57306077",
+            "urlToImage": "https://static.dw.com/image/57306135_6.jpg",
+            "publishedAt": "2021-04-23T01:59:00Z",
+            "content": "US magazine Consumer Reports said on Thursday its engineers were able to trick a Tesla into driving in the car's Autopilot mode  with no one at the wheel.\r\nThe publication's engineers were able to op… [+2079 chars]"
+        
+        }
+    ]
+}
---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+/* ケンミル */ /* 神様最高 */ "use strict"; console.log(new Date);
+
+/* DOM */
+const elems = document.querySelectorAll(".sidenav");
+const newsArticlesCont = document.querySelector(".news-articles-cont");
+
+getNews();
+
+/* Event Handlers */
+// The side navs.
+document.addEventListener('DOMContentLoaded', initSideNavs());
+
+/* Functions */
+// This is to activate the side navs.
+function initSideNavs () {
+    M.Sidenav.init(elems);
+}
+
+function renderArticle (article) {
+    newsArticlesCont.insertAdjacentHTML("beforeend", 
+    `
+        <!-- News Article -->
+        <div class="col s12 m4 l3">
+            <div class="card medium">
+                <div class="card-image">
+                    <img src="${article.urlToImage}">
+                    <span class="card-title">${article.title}</span>
+                </div>
+                <div class="card-content">
+                    <p>${article.description}</p>
+                </div>
+                <div class="card-action">
+                    <a href="${article.url}" target="_blank" class="green-text text-darken-2">Read More</a>
+                </div>
+            </div>
+        </div>
+    `);
+}
+
+function getNews () {
+    fetch("https://newsapi.org/v2/top-headlines?country=us&pageSize=32&apiKey=8fd194918cfc40c1b1da61dec0237fc3")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const dataResults = data;
+        console.log(dataResults);
+        dataResults.articles.forEach(article => {
+            renderArticle(article);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+        placeHolderArticles.articles.forEach(article => {
+            renderArticle(article);
+        });
+    });
+}
